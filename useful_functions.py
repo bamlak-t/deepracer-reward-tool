b225ec3ef178f3a021cd3431d43f04694bb5aa3f
Bamlak <54115150+bamlak-kal@users.noreply.github.com>
2022-07-18 10:49:19 +0100

    Added useful functions

---
+++ useful_functions.py
@@ -0,0 +1,14 @@
+def getTwoPointDistance(x1,x2,y1,y2):
+    return ((x1-x2)**2 + (y1-y2)**2)**0.5
+
+def getAllDistance(data):
+    total = 0
+    for i in range(len(data)-1):
+        x1 = data[i][2]
+        y1 = data[i][3]
+
+        x2 = data[i+1][2]
+        y2 = data[i+1][3]
+
+        total += getTwoPointDistance(x1,x2,y1,y2)
+    return total
